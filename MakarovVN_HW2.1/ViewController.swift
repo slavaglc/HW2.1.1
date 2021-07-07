334548798c747aa1193cc45b5dcfa79e4e7c3218
slavaglc <47712523+slavaglc@users.noreply.github.com>
2021-07-07 22:07:27 +0300

    Initial Commit

---
+++ MakarovVN_HW2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MakarovVN_HW2.1
+//
+//  Created by slava on 07.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
